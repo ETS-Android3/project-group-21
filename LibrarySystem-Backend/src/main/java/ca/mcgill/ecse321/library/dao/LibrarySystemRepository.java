35f673ac623d1b1f571d3a7ecd11837857c48e51
YuJ-Li <liyujinjin@hotmail.com>
2021-10-13 15:46:23 -0400

    setup done
    model setup done, src/main/java/dao in addition

---
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/library/dao/LibrarySystemRepository.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.library.dao;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+public class LibrarySystemRepository {
+
+}
