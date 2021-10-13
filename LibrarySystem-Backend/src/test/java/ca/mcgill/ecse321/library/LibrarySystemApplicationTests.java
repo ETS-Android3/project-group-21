c0a0ae37fbaf2e27894c9416b036cbf00cdb46da
YuJ-Li <liyujinjin@hotmail.com>
2021-10-13 11:28:49 -0400

    workflow fix
    try to fix workflow

--- LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/library/LibrarySystemApplicationTests.java
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/library/LibrarySystemApplicationTests.java
@@ -1,5 +1,7 @@
 package ca.mcgill.ecse321.library;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class LibrarySystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
