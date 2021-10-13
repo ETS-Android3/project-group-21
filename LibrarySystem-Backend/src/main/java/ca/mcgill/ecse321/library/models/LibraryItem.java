0f41396cbf1ce0b86c0a5dac5b2b21465af9f6f3
daniapenn <77757885+daniapenn@users.noreply.github.com>
2021-10-13 18:15:28 -0400

    Update LibraryItem.java

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/library/models/LibraryItem.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/library/models/LibraryItem.java
@@ -1,8 +1,28 @@
 package ca.mcgill.ecse321.library.models;
 
 public class LibraryItem {
-    public enum ItemType{
-        
-    }
+   
+  //------------------------
+  // ENUMERATIONS
+  //------------------------
+
+  public enum ItemType { Book, Movie, Music, Newspaper, Archive }
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //LibraryItem Attributes
+  private ItemType type;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public LibraryItem(ItemType aType)
+  {
+    type = aType;
+  }
+
     
 }
