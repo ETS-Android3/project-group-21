960737f301eb29ffff4908191eee36cc7687ad3e
YuJ-Li <liyujinjin@hotmail.com>
2021-10-13 10:44:28 -0400

    gradle build test
    gradle build -xtest pass

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/library/LibrarySystemApplication.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/library/LibrarySystemApplication.java
@@ -1,13 +1,21 @@
 package ca.mcgill.ecse321.library;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class LibrarySystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibrarySystemApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LibrarySystemApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
 
 }
