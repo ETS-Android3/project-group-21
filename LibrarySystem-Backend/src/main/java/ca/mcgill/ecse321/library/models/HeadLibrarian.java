b435cfa2b43697b3902df6458d4fc927af2c1887
YuJ-Li <liyujinjin@hotmail.com>
2021-10-16 18:09:54 -0400

    bug fix
    try 4

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/library/models/HeadLibrarian.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/library/models/HeadLibrarian.java
@@ -5,8 +5,8 @@ package ca.mcgill.ecse321.library.models;
 import java.util.*;
 import java.sql.Time;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 
 // line 43 "../../../../../LibrarySystem.ump"
 @Entity
