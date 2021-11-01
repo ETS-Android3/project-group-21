659f6fca5cca72e82810e99f9deae80df47fec50
kaifanzheng <kaifan.zheng@mail.mcgill.ca>
2021-11-01 02:17:47 -0400

    Citizen/headLibrarian/Librarian Service class created
    Citizen/headLibrarian/Librarian Service class created

---
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/library/service/HeadLibrarianService.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.library.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ca.mcgill.ecse321.library.dao.*;
+import ca.mcgill.ecse321.library.models.*;
+
+public class HeadLibrarianService {
+
+}
