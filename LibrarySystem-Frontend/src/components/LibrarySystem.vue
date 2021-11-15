2e92a9aa6148da3a8c0b1ac9b171595cf8690a95
YuJ-Li <liyujinjin@hotmail.com>
2021-11-15 15:15:22 -0500

    frontend setup
    frontend setup and added the templated demonstrated in tutorial

---
+++ LibrarySystem-Frontend/src/components/LibrarySystem.vue
@@ -0,0 +1,33 @@
+
+<!--This is a template from the tutorial, plz do not just copy paste it for the other vue -->
+<template>
+    <div id="librarysystem">
+        <h2>Citizen</h2>
+        <table>
+            <tr>
+                <td>Bob</td>
+                <td>Reservation</td>
+            </tr>
+            <tr>
+                <td>
+                    <input libraryitem="item" applicationuser="User Name">
+                </td>
+                <td>
+                    <button>Create</button>
+                </td>
+            </tr>
+        </table>
+        <p>
+            <span style="color:red">Error: Message text comes here </span>
+        </p>
+    </div> 
+</template>
+<script>
+</script>
+<style>
+    #librarysystem{
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        color: #2c3e50;
+        background: #f2ece8;
+    }
+</style>
