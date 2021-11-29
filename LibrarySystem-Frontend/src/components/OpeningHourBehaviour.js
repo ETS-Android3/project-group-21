99b0433afc62273ba4104f0d3fc89458fa3f2d94
dania.pennimpede@mail.mcgill.ca <dania.pennimpede@mail.mcgill.ca>
2021-11-29 14:56:45 -0500

    Buttons Add, Update and Delete Opening Hour work

--- LibrarySystem-Frontend/src/components/OpeningHourBehaviour.js
+++ LibrarySystem-Frontend/src/components/OpeningHourBehaviour.js
@@ -25,6 +25,7 @@ export default {
 			startTime:'',
 			endTime:'',
 			
+			deleteDay:'',
 			openinghours:[],
 			newOpeningHour:'',
 			errorOpeningHour:'',
@@ -34,7 +35,7 @@ export default {
 	
 	created: function (){
 		 //Initializing citizens from backend
-        AXIOS.get('/openinghour')
+        AXIOS.get('/openinghours')
         .then(response => {
         //JSON responses are automatically parsed
         	this.openinghours = response.data
@@ -44,13 +45,40 @@ export default {
         })
 		
 		// Test data
-		const o1 = new OpeningHourDto('Monday', '10:30:00', '11:30:00')
-		const o2 = new OpeningHourDto('Tuesday', '10:30:00', '11:30:00')
+		//const o1 = new OpeningHourDto('Monday', '10:30:00', '11:30:00')
+		//const o2 = new OpeningHourDto('Tuesday', '10:30:00', '11:30:00')
 		// Sample initial content
-		this.openinghours = [o1, o2]
+		//this.openinghours = [o1, o2]
 	},
 	
 	methods: {
+		createOpeningHour: function (day, startTime, endTime) {
+			AXIOS.post('/openinghours/'.concat(day), {}, 
+			{params:{
+				day: day,
+				startTime: startTime,
+				endTime: endTime
+			}})
+			
+			.then(response => {
+				// JSON responses are automatically parsed.
+				this.openinghours.push(response.data)
+				this.day=''
+				this.startTime=''
+				this.endTime=''
+				})
+				.catch(e => {
+					var errorMsg = e.response.data.message
+					console.log(errorMsg)
+					this.errorOpeningHour = errorMsg
+					})
+			// Create a new openinghour and add it to the list of openinghours
+			//var o = new OpeningHourDto(day, startTime, endTime)
+			//this.openinghours.push(o)
+			// Reset the name field for new openinghours
+			this.newOpeningHour = ''
+			},
+		
 	    updateOpeningHour: function(day, startTime, endTime){
 			AXIOS.patch('/openinghours/'.concat(day), {},
 			 {params:{
@@ -65,66 +93,40 @@ export default {
 					this.openinghours = response.data
 					})
 					.catch(e => {
-						this.errorOpeningHour = e
+					this.errorOpeningHour = e
 					})
-					this.newOpeningHour=''
-					this.errorOpeningHour=''
+					this.day=''
+					this.startTime=''
+					this.endTime=''
 				})
 				.catch(e =>{
 					var errorMsg = e.response.data.message
             		console.log(errorMsg)
             		this.errorOpeningHour = errorMsg
 				})
-			this.newOpeningHour=''
 		},
 		
-		deleteOpeningHour: function(day){
-			AXIOS.delete('/openinghours/'.concat(day), {}, {})
+		deleteOpeningHour: function(deleteDay){
+			AXIOS.delete('/openinghours/'.concat(deleteDay), {}, {})
 				.then(response => {
 					AXIOS.get('/openinghours')
 					.then(response => {
 						//JSON responses are automatically parsed
 						this.openinghours = response.data
+						this.day=''
+						this.startTime=''
+						this.endTime=''
+						this.deleteDay=''
 					})
 					.catch(e =>{
-						this.errorOpeningHour = e
+					this.errorOpeningHour = e
 					})
-					
-					this.newOpeningHour =''
-					this.errorOpeningHour = ''
 				})
 				.catch(e => {
-					var errorMsg = e.response.data.message
-					console.log(errorMsg)
-					this.errorOpeningHour = errorMsg
+				this.errorOpeningHour=e
         	})
-        	this.newOpeningHour =''
-		},
+		}
 		
-	    createOpeningHour: function (day, startTime, endTime) {
-			AXIOS.post('/openinghour/'.concat(day), {}, 
-			{params:{
-				day: day,
-				startTime: startTime,
-				endTime: endTime
-			}})
-			
-			.then(response => {
-				// JSON responses are automatically parsed.
-				this.openinghours.push(response.data)
-				this.newOpeningHour = ''
-				this.errorOpeningHour = ''
-				})
-				.catch(e => {
-					var errorMsg = e.response.data.message
-					console.log(errorMsg)
-					this.errorOpeningHour = errorMsg
-					})
-			// Create a new openinghour and add it to the list of openinghours
-			var o = new OpeningHourDto(day, startTime, endTime)
-			this.openinghours.push(o)
-			// Reset the name field for new openinghours
-			this.newOpeningHour = ''
-			}
+	    
 		}
 }
