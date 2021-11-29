466eb132e43dc9bdf516f67aec09e8dd3949539f
joris-afk <joris.ah-kane@mail.mcgill.ca>
2021-11-29 00:43:52 -0500

    Made the shift page pretty
    Only gotta add links to it on librarian and head librarian

--- LibrarySystem-Frontend/src/components/Shift.vue
+++ LibrarySystem-Frontend/src/components/Shift.vue
@@ -2,26 +2,26 @@
     <div class="shift">
         <h2>Shifts</h2>
 
-        <table>
-            <tr>
+        <table class="tablestyle" align="center">
+            <tr class="trstyle">
                 <td> shiftCode </td>
                 <td> startTime </td>
                 <td> endTime </td>
                 <td> day </td>
-                <!-- <td> Associated user </td> -->
+                <td> Associated user </td> <!---->
             </tr>
-            <tr v-for="shift in shifts" :key="shift.shiftCode">
+            <tr class="trstyle" v-for="shift in shifts" :key="shift.shiftCode">
                 <td>{{ shift.shiftCode }}</td>
                 <td>{{ shift.startTime }}</td>
                 <td>{{ shift.endTime }}</td>
                 <td>{{ shift.day }}</td>
-                <!-- <td>{{ shift.applicationUser.name }}</td> -->
+                <td>{{ shift.applicationUser.name }}</td> <!---->
             </tr>
         </table>
-        <table>
+        <table align="center">
             <tr>
                 <th><label>Shift Code:</label></th>
-                <td><input type="text" v-model="shiftCode" placeholder="shift code"></td>
+                <td><input type="text" v-model="shiftCode" placeholder="Shift code"></td>
             </tr>
             <tr>
                 <th><label>Start Time:</label></th>
@@ -39,14 +39,16 @@
                 <th><label>Associated Employee:</label></th>
                 <td><input type="text" v-model="applicationUser.cardID" placeholder="User Id"></td>
             </tr>
+        </table>
+        <table align="center">
             <tr>
-                <button v-bind:disabled="(!shiftCode || !startTime || !endTime || !day || !applicationUser.cardID)" 
+                <button class="btn btn-primary" v-bind:disabled="(!shiftCode || !startTime || !endTime || !day || !applicationUser.cardID)" 
                 @click="createShift(shiftCode,  startTime,  endTime,  day,  applicationUser.cardID)">Add New Shift</button>
                 &nbsp;
-                <button v-bind:disabled="(!shiftCode || !startTime || !endTime || !day || !applicationUser.cardID)" 
+                <button class="btn btn-primary" v-bind:disabled="(!shiftCode || !startTime || !endTime || !day || !applicationUser.cardID)" 
                 @click="updateShift(shiftCode,  startTime,  endTime,  day,  applicationUser.cardID)">Update a shift</button>
                 &nbsp;
-                <button v-bind:disabled="!shiftCode" 
+                <button class="btn btn-primary" v-bind:disabled="!shiftCode" 
                 @click="deleteShift(shiftCode)">Remove Shift</button>
             </tr>
         
@@ -65,4 +67,32 @@
     color: #2c3e50;
     background: #f2ece8;
   }
+  .btn-primary:disabled{
+    padding: 10px 20px;
+    font-size: 12px;
+    border-radius: 10px;
+    background-color: #42b983;
+}
+.btn-primary{
+    padding: 10px 20px;
+    font-size: 12px;
+    border-radius: 10px;
+    background-color: #42b983;
+}
+
+.tablestyle {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 70%;
+}
+
+.tdstyle, .thstyle {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+.trstyle:nth-child(even) {
+  background-color: #dddddd;
+}
 </style>
