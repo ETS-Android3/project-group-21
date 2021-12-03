2b197cbb4effe165f2bfa90b5696733d8fc4c91a
Chun160712 <chun160712@gmail.com>
2021-12-02 20:40:26 -0500

    Implement add library item feature for Android App
    - Test not done yet due to the problem with heroku application (cannot use SDK manager)

--- LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/MainActivity.java
+++ LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/MainActivity.java
@@ -3,6 +3,8 @@ package ca.mcgill.ecse321.librarysystem_android;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,14 +16,23 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import ca.mcgill.ecse321.librarysystem_android.databinding.ActivityMainBinding;
+import cz.msebera.android.httpclient.Header;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String error = null;
+
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        refreshErrorMessage();
     }
 
     @Override
@@ -72,5 +84,38 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void addLibraryItem(View v) {
+        error = "";
+        final TextView tv = (TextView) findViewById(R.id.newlibraryitem_barcode);
+        HttpUtils.post("libraryitems/" + tv.getText().toString(), new RequestParams(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                tv.setText("");
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
     }
 }
